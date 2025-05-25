fd1d8a9a80e8cf06d5aeca7b403dc1d4ef0ae5c7
Hanzzz123 <aanzs496@gmail.com>
2025-05-25 23:04:59 +0700

    final touch 2

--- src/test/java/com/example/PeriplusCartTest.java
+++ src/test/java/com/example/PeriplusCartTest.java
@@ -37,8 +37,6 @@ public class PeriplusCartTest {
 
     @BeforeMethod
     public void setUp() throws MalformedURLException {
-        WebDriverManager.chromedriver().setup();
-
         //testing locally (comment this if testing portable with docker)
         //WebDriverManager.chromedriver().setup();
         //driver = new ChromeDriver();
